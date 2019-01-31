6a28a51a3a5dda99238b3446d8aff33f35d20f7a
Matt Arcidy <marcidy@gmail.com>
2019-01-31 10:29:24 -0800

    adding documentation

--- pwm_test/chemrap.cpp
+++ pwm_test/chemrap.cpp
@@ -3,24 +3,47 @@
 #include <cmath>
 
 
+//! Constructor, assigns the TWI bus used to the object.
 Chemrap::Chemrap() {
     i2cBus = &Wire;
 }
 
+//! initializes the is2 bus, starts the pwm driver, and sets the default frequency
 void Chemrap::begin(void) {
     
     i2cBus->begin(SDA, SCL);
     delay(50);
     writeByteTo(MODE1, 0x80);
-    setPrescale(60); // default frequency
+    setPrescale(60); // default frequency for servos ~ 60Hz
 }
 
+//! resets the pwm driver and turns it on
 void Chemrap::reset(void) {
 
     writeByteTo(MODE1, 0x80);
     delay(10);
 }
 
+/*!
+ Sets the angle for a specific motor attached to the pwm driver.
+ The motorReg register is the first of 4 registered associated with setting the 
+ 12 bit DAC values.  Two 8 bit registers for the "on" pulse start and two
+ for the "off" pulse end.  The motor registers start at address SERVO_REG_BASE.
+
+ The servos are calibrated with a SERVO_MIN and SERVO_MAX value which determine the 
+ values between which a speciifc motor will turn.  While these values are relative to
+ each other, absolute numbers are chosen during calibration starting with 0.
+
+ The motors will not turn wihout a minimum pulse width, and there is a risk of
+ lockup witout it.  When the pulse width is calculated, this is added on regardless
+ of value.
+
+ The start registser and pulse location are passed to setMotorPwm.
+
+ \sa setMotorPwm
+ \param motor an integer representing a port on the pwm chip, 0-15
+ \param angle a float in 0.0-180.0
+ */
 void Chemrap::setAngle(int motor, float angle) {
 
     uint8_t motorReg    = SERVO_REG_BASE + 4*motor;
@@ -30,6 +53,7 @@ void Chemrap::setAngle(int motor, float angle) {
     setMotorPwm(motorReg, on, off);
 }
 
+//! Sets the prescale value for the pwm driver.  See datasheet.
 void Chemrap::setPrescale(float freq) {
 
     frequency = freq;
@@ -46,14 +70,15 @@ void Chemrap::setPrescale(float freq) {
     delay(10);
 }
 
-void Chemrap::writeByteTo(uint8_t reg, uint8_t val) {
+/*! 
+ Sets the PWM pulse width for a motor corresponding to the 4 registers beginning at motorReg.
+ This function uses autoincrement to write 4 bytes in sequence to the registers for a motor.
+ See the datasheet for the PWM controller.
 
-    i2cBus->beginTransmission(ADDRESS);
-    i2cBus->write(reg);
-    i2cBus->write(val);
-    i2cBus->endTransmission();
-}
-
+ \param motorReg the start of 4 sequential registers holding the start and end pulse values
+ \param on a 16bit value which holds the 12bit DAC start value for the pwm pulse
+ \param on a 16bit value which holds the 12bit DAC end value for the pwm pulse
+*/
 void Chemrap::setMotorPwm(uint8_t motorReg, uint16_t on, uint16_t off) {
 
     // clean this up
@@ -62,9 +87,9 @@ void Chemrap::setMotorPwm(uint8_t motorReg, uint16_t on, uint16_t off) {
     uint8_t offLow  = (uint8_t)off;
     uint8_t offHigh = (uint8_t)(off>>8);
 
-    uint8_t mode1 = readByteFrom(MODE1);
+    uint8_t mode1 = readByteFrom(MODE1);    //< retrieve and store MODE1 register values
     
-    writeByteTo(MODE1, mode1 | 0xA0); // turn on autoincrement
+    writeByteTo(MODE1, mode1 | 0xA0);       //< turn on autoincrement
 
     i2cBus->beginTransmission(ADDRESS);
     i2cBus->write(motorReg);
@@ -74,10 +99,11 @@ void Chemrap::setMotorPwm(uint8_t motorReg, uint16_t on, uint16_t off) {
     i2cBus->write(offHigh);
     i2cBus->endTransmission();
 
-    writeByteTo(MODE1, mode1 | 0x80);  // turn off autoincrement
+    writeByteTo(MODE1, mode1 | 0x80);       //< restore MODE1 and turn off autoincrement
 
 }
 
+//! helper function to read 1 byte only from a register
 uint8_t Chemrap::readByteFrom(uint8_t reg) {
 
     i2cBus->beginTransmission(ADDRESS);
@@ -86,6 +112,15 @@ uint8_t Chemrap::readByteFrom(uint8_t reg) {
     i2cBus->requestFrom(ADDRESS, 1);
 
     return i2cBus->read();
+}
+
+//! Helper function to write 1 byte only to a register
+void Chemrap::writeByteTo(uint8_t reg, uint8_t val) {
+
+    i2cBus->beginTransmission(ADDRESS);
+    i2cBus->write(reg);
+    i2cBus->write(val);
+    i2cBus->endTransmission();
 }
 
 void Chemrap::info(void) {
--- pwm_test/pwm_test.ino
+++ pwm_test/pwm_test.ino
@@ -8,64 +8,14 @@
 #define SCL 33
 
 Chemrap chemrap = Chemrap();
-
     
 void setup() {
     Serial.begin(115200);
     chemrapSetup();
-    // servoDriverSetup();
 }
 
 void loop() {
     chemrapTest();
-    // servoDriverTest();
-}
-
-
-void servoDriverSetup() {
-    Wire.begin(SDA, SCL);
-    delay(10);
-    writeByte(0x00, 0x80);
-    delay(50);
-    uint8_t mode1 = readByte(0x00);
-    uint8_t sleep = (mode1 & 0x7F) | 0x10;
-    writeByte(0x00, sleep);
-    writeByte(0xFE, 101);  // 101
-    writeByte(0x00, mode1);
-    delay(1000);
-}
-
-void servoDriverTest() {
-    uint8_t onLow=0x42;
-    uint8_t onHigh=0x43;
-    uint8_t offLow=0x44;
-    uint8_t offHigh=0x45;
-
-    for (uint16_t low=0x02BC; low>0x0000; --low) {
-        Serial.print("Low");    Serial.print(" ");
-        Serial.print((uint8_t)(low>>8), HEX);    Serial.print(" ");
-        Serial.print((uint8_t)low, HEX);         Serial.print(" ");
-        Serial.print((uint8_t)(0x02BC>>8), HEX); Serial.print(" ");
-        Serial.print((uint8_t)0x02BC, HEX);      Serial.println(" ");
-        writeByte(onHigh,   (uint8_t)(low>>8));
-        writeByte(onLow,    (uint8_t)low);
-        writeByte(offHigh,  (uint8_t)(0x02BC>>8));
-        writeByte(offLow,   (uint8_t)0x02BC);
-        delay(50);
-    }
-
-    for (uint16_t high=0x001D; high<=0x02BC; high++) {
-        Serial.print("Low");    Serial.print(" ");
-        Serial.print((uint8_t)(0x001D>>8), HEX);    Serial.print(" ");
-        Serial.print((uint8_t)0x001D, HEX);         Serial.print(" ");
-        Serial.print((uint8_t)(high>>8), HEX); Serial.print(" ");
-        Serial.print((uint8_t)high, HEX);      Serial.println(" ");
-        writeByte(onLow,    0x1D);
-        writeByte(onHigh,   0x00);
-        writeByte(offLow,   high);
-        writeByte(offHigh,  high>>8);
-        delay(50);
-    }
 }
 
 void chemrapSetup() {
@@ -76,8 +26,8 @@ void chemrapSetup() {
 }
 
 void chemrapTest() {
-    int motor = 15;
 
+    // cycle through full turn for 4 motors, ports 12-15 on the pwm board
     for(int angle=10; angle<=170; angle++) {
         Serial.print(angle);Serial.println("  ");
         chemrap.setAngle(15, angle);
@@ -86,20 +36,4 @@ void chemrapTest() {
         chemrap.setAngle(12, angle);
         delay(100);
     }
-}
-
-void writeByte(uint8_t addr, uint8_t val) {
-    Wire.beginTransmission(ADDRESS);
-    Wire.write(addr);
-    Wire.write(val);
-    Wire.endTransmission();
-}
-
-uint8_t readByte(uint8_t addr) {
-    Wire.beginTransmission(ADDRESS);
-    Wire.write(addr);
-    Wire.endTransmission();
-
-    Wire.requestFrom(ADDRESS, 1);
-    return Wire.read();
 }
